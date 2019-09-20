bc98bd078aa4a42c68ecf6445259416da04c1f59
AT <anasst87@gmail.com>
2019-09-20 13:01:08 +0200

    5th commit

--- fichier1.c
+++ fichier1.c
@@ -24,7 +24,7 @@ int* fusion(int t1[],int t2[],int size1, int size2){
         i++;
         it1++;
     }
-    while (it2 < size2) //8888
+    while (it2 < size2) //88886
     {
         fus[i]=t2[it2];
         i++;
