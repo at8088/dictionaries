d7a90b6a9b8c45d198633c657bd0e3022767dd15
AT <anasst87@gmail.com>
2019-09-23 18:36:04 +0200

    da7k

---
+++ hehe.c
@@ -0,0 +1,7 @@
+#include "hehe.h"
+
+fuck you and your
+coding 
+    style i_do
+    what i want
+get_fucked
---
+++ hehe.h
@@ -0,0 +1,13 @@
+#define fuck int 
+#define you main 
+#define and ( 
+#define your ) 
+#define coding {
+#define style printf(
+#define  i_do "I AM A MAD MAN\nDONT @ ME\nMISS ME WITH THAT DUMB SHIT\n");
+#define want ;
+#define i 0
+#define  what return 
+#define get_fucked }
+
+#include <stdio.h>
