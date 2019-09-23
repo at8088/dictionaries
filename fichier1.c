2fb5937138136bcfc3bf2057e3629c1021bd898b
AT <anasst87@gmail.com>
2019-09-23 18:40:20 +0200

    ik'hg

--- fichier1.c
+++ fichier1.c
@@ -5,28 +5,23 @@
 int* fusion(int t1[],int t2[],int size1, int size2){
     int it1=0,it2=0,i=0;
     int  fus[size1+size2] ;
-    while (it1 < size1 && it2 < size2)
-    {
-        if (t1[it1]<t2[it2])
-        {
+    while (it1 < size1 && it2 < size2){
+        if (t1[it1]<t2[it2]){
             fus[i]=t1[it1];
             i++;
             it1++;
-        }else
-        {
+        }else{
             fus[i]=t2[it2];
             i++;
             it2++;
         }
     }
-    while (it1 < size1)
-    {
+    while (it1 < size1){
         fus[i]=t1[it1];
         i++;
         it1++;
     }
-    while (it2 < size2) 
-    {
+    while (it2 < size2) {
         fus[i]=t2[it2];
         i++;
         it2++;
@@ -40,15 +35,21 @@ void affiche(int t[],int n){
     }
     puts("");
 }
-void tri_fusion(int t[] , int n){
+void tri_fusion_bis(int t[] , int n){
     if (n<=1)
     {
         return;
     }
-    tri_fusion(t,n/2);
-    tri_fusion(t+n/2+1,n-n/2);
+    tri_fusion_bis(t,n/2);
+    tri_fusion_bis(t+n/2,n-n/2);
     t=fusion(t,t+n/2,n/2,n/2);
 }
+void tri_fusion(int t[], int n){
+    if(n>0){
+        tri_fusion_bis(t,n);
+    }
+}
+
 bool rech_dicho(int t[],int n,int val){
     if (n==0)
     {
@@ -71,10 +72,10 @@ int main(int argc, char const *argv[]){
     int l[7]={3,69,17,0,10,11,12};
     int p = 10/6;
     //printf("%d\n",p);
-   // tri_fusion(l,7);
-    bool b = rech_dicho(t,5,8);
-    printf("%d\n",b);
-    //affiche(l,7);
+   tri_fusion(l,7);
+    // bool b = rech_dicho(t,5,8);
+    // printf("%d\n",b);
+    affiche(l,7);
     //affiche(fus,12);
    // free(t);
     return 0;
