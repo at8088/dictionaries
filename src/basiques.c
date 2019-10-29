cd26de776b99c266564937d408ad2374e8ef06a9
at8088 <anasst87@gmail.com>
2019-10-29 12:08:39 +0100

    hjk

---
+++ src/basiques.c
@@ -0,0 +1,28 @@
+#include "fct-primitives.h"
+
+/*************versions iteratives*********/
+
+
+bool contains_iter(dico d, char * word, unsigned size){
+
+}
+/* size est la taille du mot word */
+bool add_iter(dico d, char * word, unsigned size){
+
+}
+bool remove_iter(dico d, char * word, unsigned size){
+
+}
+
+
+/*************versions recursives*********/
+
+bool contains_rec(dico d, char * word, unsigned size){
+
+}
+bool add_rec(dico d, char * word, unsigned size){
+
+}
+bool remove_rec(dico d, char * word, unsigned size){
+
+}
--- src/fct-primitives.h
+++ src/fct-primitives.h
@@ -39,7 +39,14 @@ void print_prefix(dico d);
 bool equals(dico d1, dico d2);
 
 
+bool contains_iter(dico d, char * word, unsigned size);
+/* size est la taille du mot word */
+bool add_iter(dico d, char * word, unsigned size);
+bool remove_iter(dico d, char * word, unsigned size);
 
+bool contains_rec(dico d, char * word, unsigned size);
+bool add_rec(dico d, char * word, unsigned size);
+bool remove_rec(dico d, char * word, unsigned size);
 
 #endif
 
