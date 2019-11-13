f005fa23136fa0f52856192853b8f16971c1bcb1
Anass Tahiri <tahiria@ensipc438.ensimag.fr>
2019-11-13 09:07:27 +0100

    iterateur

---
+++ src/iterator.h
@@ -0,0 +1,20 @@
+#include "fct-primitives.h"
+
+struct iterator_info {
+    tree t;
+    int index_word;
+};
+
+struct _iterator {
+    char * word;
+    struct iterator_info * stack;
+    int index_stack;
+};
+
+typedef struct _iterator iterator;
+
+
+iterator * start_iterator(dico d);
+void destroy_iterator(iterator ** it);
+bool has_next(iterator * it);
+char * next (iterator * it);
